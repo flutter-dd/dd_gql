2d1902a1f70f7a6e91f6d92a7707b9429ea5769a
duanchnaghe <592110272@qq.com>
2021-01-08 15:27:43 +0800

    0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,2 @@
 ## [0.0.1] - First update.
+## [0.0.2] - Fix_bug update.
--- lib/dd_gql.dart
+++ lib/dd_gql.dart
@@ -3,6 +3,7 @@ library dd_gql;
 import 'package:artemis/client.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart';
+import 'package:artemis/client.dart';
 import 'package:artemis/artemis.dart';
 
 part 'src/gql_api.dart';
